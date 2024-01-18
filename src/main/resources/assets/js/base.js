b1d2cfaac8586763e4b167def0d2ce9e8ad9b3d9
Asiel Leal Celdeiro <15990580+lealceldeiro@users.noreply.github.com>
2024-01-18 23:49:03 +0200

    fix: On mobile navbar transparency interleaves with page content
    - Fixes #23

--- src/main/resources/assets/js/base.js
+++ src/main/resources/assets/js/base.js
@@ -1,11 +1,32 @@
 const navEl = document.querySelector("#menuNav");
+
+let navShown = false;
+function handleNavBarToggle() {
+    if (!navShown) {
+        showMenuNavbarShadow();
+    }
+    navShown = !navShown;
+}
+
+function showMenuNavbarShadow() {
+    navEl.classList.add("navbar-scrolled");
+    navEl.classList.add("bg-light-subtle");
+}
+
+function hideMenuNavbarShadow() {
+    navEl.classList.remove("navbar-scrolled");
+    navEl.classList.remove("bg-light-subtle");
+}
+
 window.addEventListener("scroll", () => {
+    if (navShown) {
+        document.querySelector("#navbarSupportedContent").classList.remove("show");
+        navShown = false;
+    }
     const threshold = 18;
     if (window.scrollY >= threshold) {
-        navEl.classList.add("navbar-scrolled");
-        navEl.classList.add("bg-light-subtle");
+        showMenuNavbarShadow();
     } else if (window.scrollY < threshold) {
-        navEl.classList.remove("navbar-scrolled");
-        navEl.classList.remove("bg-light-subtle");
+        hideMenuNavbarShadow();
     }
 });
